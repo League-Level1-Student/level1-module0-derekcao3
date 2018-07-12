02be5bede2fac79760f2bc0942f88a526393f40a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-11 17:01:20 -0700

    PhotoQuiz, GUI

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,31 +15,31 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
-		//2. Set your JFrame object to be visible
-
+		JFrame frame = new JFrame();
+		//2. Set your JFram		e object to be visible
+		frame.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel jacob = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel jacobs = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
-		//8. Add the JPanel object to the JFrame
-
+		jacobs.setText("For score and seven years ago out fathers... ");
+		//8. Add the JPanel object to the JFrame	
+		frame.add(jacob);
 		//9. Add the JLabel object to the JPanel
-
+		jacob.add(jacobs);
 		//10. Pack your JFrame.
-
+		frame.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+		jacobs.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
+		frame.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,23 +22,28 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+        String image = "https://i.kym-cdn.com/entries/icons/mobile/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+        Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+        component = createImage(image);
 		// 4. add the image to the quiz window
-
+        quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+        quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+        String yes = JOptionPane.showInputDialog("despacito 2?");
+        System.out.println(yes);
 		// 7. print "CORRECT" if the user gave the right answer
-
+        if(yes=="yes") {
+        	JOptionPane.showMessageDialog(null, "CORRECT");
+        }
 		// 8. print "INCORRECT" if the answer is wrong
-
+        else {
+        	JOptionPane.showMessageDialog(null, "INCORRECT");
+        }
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+        
 		// 10. find another image and create it (might take more than one line of code)
 
 		// 11. add the second image to the quiz window

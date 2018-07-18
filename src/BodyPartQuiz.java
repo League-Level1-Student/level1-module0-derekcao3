bea0742fb2c6e1d4be85822af5c26dad3479e76a
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-18 16:59:26 -0700

    Drum Kit, Body Part Quiz

--- src/BodyPartQuiz.java
+++ src/BodyPartQuiz.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -15,42 +16,70 @@ import javax.swing.JPanel;
 
 public class BodyPartQuiz {
 
-	// 0. You can use the celebrity photos we have placed in the default java package,  
-	// or if you prefer, get celebrity photos from the Internet, place them in the default
+	// 0. You can use the celebrity photos we have placed in the default java
+	// package,
+	// or if you prefer, get celebrity photos from the Internet, place them in the
+	// default
 	// package, and change the names below.
-	
+
 	String firstImage = "src/arnold.jpeg";
 	String secondImage = "src/leonardo.jpeg";
 	String thirdImage = "src/morgan.jpeg";
 	String fourthImage = "src/jack.jpeg";
-	
+
 	JFrame window = new JFrame();
 	JPanel panel = new JPanel();
-
 
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
-
+		int score = 0;
 		// 2. Set the size of the window in the initializeGui() method below
-
+		JOptionPane.showMessageDialog(null, "guess the celebrity's first name(all lowercase)");
+		initializeGui();
 		for (int i = 0; i < 4; i++) {
-			
+
 			// 4. Ask the user who this person is and store their answer
-			String guess= JOptionPane.showInputDialog("who is this?");
-			
+			String guess = JOptionPane.showInputDialog("who is this?");
+
 			// 5. Check their answer. If they guessed correctly:
 			// -- Tell them they are right and increase the score by 1
-
+			if(guess.equals("arnold") & i==0) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score=score+1;
+			}
 			// 6. Otherwise:
 			// -- Tell them they are wrong and who the person is
-
+			else if(i==0) {
+				JOptionPane.showMessageDialog(null, "Incorrect, it's arnold");
+			}
 			// 7. Use the showNextImage() method below to get the next image
 			showNextImage();
-		    // 8. Show them their current score
+			// 8. Show them their current score
 			
 			// 9. .... repeat for all your images.....
-
+			if(guess.equals("leonardo") & i==1) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score=score+1;
+			}
+			else if(i==1) {
+				JOptionPane.showMessageDialog(null, "Incorrect, it's leonardo");
+			}
+			if(guess.equals("morgan") & i==2) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score=score+1;
+			}
+			else if(i==2) {
+				JOptionPane.showMessageDialog(null, "Incorrect, it's morgan");
+			}
+			if(guess.equals("jack") & i==3) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score=score+1;
+			}
+			else if(i==3) {
+				JOptionPane.showMessageDialog(null, "Incorrect, it's jack");
+			}
+			JOptionPane.showMessageDialog(null, "Score: "+ score);
 
 		}
 
@@ -58,7 +87,7 @@ public class BodyPartQuiz {
 
 	public void showNextImage() {
 		panel.removeAll();
-		panel.add(getNextImage());		
+		panel.add(getNextImage());
 		window.setVisible(true);
 	}
 
@@ -73,12 +102,12 @@ public class BodyPartQuiz {
 		imageIterator = imageList.iterator();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.add(panel);
-		
-		// 3. Change the size of the window so that you can only see part of the image.		
-		window.setSize(500,500);
-		
+
+		// 3. Change the size of the window so that you can only see part of the image.
+		window.setSize(100, 100);
+
 		showNextImage();
-		
+
 	}
 
 	private JLabel loadImage(String fileName) {
--- src/DrumKit.java
+++ src/DrumKit.java
@@ -21,51 +21,69 @@ import javax.swing.JPanel;
 public class DrumKit implements MouseListener {
 
     JLabel drumLabelWithImage;
-
+    JLabel drumLabelWithImage2;
+    
     public static void main(String[] args) throws Exception {
    	 new DrumKit().getGoing();
     }
 
     private void getGoing() throws MalformedURLException {
    	 // 1. Make a JFrame variable and initialize it using "new JFrame()"
-   	 
+   	 JFrame frame = new JFrame();
    	 // 2. Make the frame visible and 
     //     set its default close operation to JFrame.EXIT_ON_CLOSE
-   	 
+   	 frame.setVisible(true);
+   	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
    	 // 3. Set the size of the frame
-   	 
+   	 frame.setSize(300, 300);
    	 // 4. Set the title of the frame
-   	 
+   	 frame.setTitle("JFrame");
    	 // 5. Make a JPanel variable and initialize it using "new JPanel().
-   	 
+   	 JPanel panel = new JPanel();
    	 // 6. Add the panel to the frame. (The panel is invisible.)
-    
+    frame.add(panel);
    	 // 7. Download an image of a drum from the Internet. Drop it into your Eclipse project under "default package".
    	 // 8. Put the name of your image in a String variable.
-    
+    String drum = "drum.jpeg";
    	 // 9. Edit the next line to use your String variable
 // drumLabelWithImage = createLabelImage(drumImageString);
-   	 
+   	 drumLabelWithImage = createLabelImage(drum);
    	 // 10. Add the image to the panel
-    
+    panel.add(drumLabelWithImage);
    	 // 11. Set the layout of the panel to "new GridLayout()"
-    
+    panel.setLayout(new GridLayout());
       	 // 12. call the pack() method on the frame.  Run your program. Do you see your drum image?
-   	  
+   	  	frame.pack();
    	 // 13. add this mouse listener to drumLabelWithImage
-    	
+    	drumLabelWithImage.addMouseListener(this);
      	 // 18. Add more images to make a drumkit. Remember to add this mouse listener to each one.
-
+    	String drum2 = "drum2.jpeg";
+      	 drumLabelWithImage2 = createLabelImage(drum2);
+      	 panel.add(drumLabelWithImage2);
+      	panel.setLayout(new GridLayout());
+      	drumLabelWithImage2.addMouseListener(this);
+      	frame.pack();
     }
 
     public void mouseClicked(MouseEvent e) {
    	 // 14. Print "mouse clicked" to the console. Run your program and watch the console to see when this is printed.
-
+  
    	 JLabel drumClicked = (JLabel) e.getSource();  // This line gets the label that the mouse clicked on
-   	 
+   	 if(drumClicked == drumLabelWithImage) {
+   		 System.out.println("mouse clicked");
+   	 }
+   	JLabel drumClicked2 = (JLabel) e.getSource();
+   	 if(drumClicked2 == drumLabelWithImage2) {
+   		 System.out.println("mouse clicked");
+   	 }
    	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
    	 // 16. If they clicked on the drumImage...
-
+   	 if(drumClicked == drumLabelWithImage) {
+   		 playSound("drum.wav");
+   	 }
+   	 if(drumClicked2 == drumLabelWithImage2) {
+   		 playSound("drum2.wav");
+   	 }
    		 // 17. ...use the playSound method to play a drum sound. Test to see if it works
 
  
